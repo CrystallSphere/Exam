06f658841a16ab499738dc7dc6c6dddd31ae8168
CrystallSphere <53644790+CrystallSphere@users.noreply.github.com>
2019-08-04 21:08:08 +0300

    Update Exam.java

--- Exam/src/Exam.java
+++ Exam/src/Exam.java
@@ -3,14 +3,23 @@ import java.util.Random;
 
 public class Exam {
     public static void main(String [] args) {
-        int start = -50;
-        int end = 50;
+        int [] a = {-1, -2,-3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20, -21, -22,
+                -23, -24, -25, -26, -27, -28, -29, -30, -31, -32, -33, -34, -35, -36, -37, -38, -39, -40, -41, -42, -43,
+                -44, -45, -46, -47, -48, -49, -50};
         int[] mas = new int[100];
         Random rand = new Random();
         for (int i = 0; i < mas.length; i++)
-            mas[i] = rand.nextInt(100);
+            mas[i] = rand.nextInt(100) -50;
         for (int i = 0; i < mas.length; i++)
             System.out.print(mas[i] + " ");
+        for (int value : a) {
+            if (value < 0) {
+                System.out.println(value);
+            }
+
+
+
+        }
     }
 
 }
